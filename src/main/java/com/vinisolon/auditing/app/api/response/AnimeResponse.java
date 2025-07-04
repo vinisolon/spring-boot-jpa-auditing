49bdae352fd31129bbaea65f1062f3b744841516
Vinicius Solon <vini.97@live.com>
2025-07-04 09:44:12 -0300

    Update resquest and response

--- src/main/java/com/vinisolon/auditing/app/api/response/AnimeResponse.java
+++ src/main/java/com/vinisolon/auditing/app/api/response/AnimeResponse.java
@@ -1,15 +1,21 @@
 package com.vinisolon.auditing.app.api.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AnimeResponse {
+public class AnimeResponse implements Serializable {
     private Long id;
     private String title;
     private String authorName;
@@ -17,6 +23,8 @@ public class AnimeResponse {
     private Integer releaseYear;
     private Boolean finished;
     private Integer seasons;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime createdAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime updatedAt;
 }
