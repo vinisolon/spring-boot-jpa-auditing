a8d2a2bdcbdbeb21803c47a10cdb8ea4bda45d69
Vinicius Solon <vini.97@live.com>
2025-07-04 07:41:03 -0300

    Init project

---
+++ src/main/java/com/vinisolon/auditing/app/SpringBootJpaAuditingApplication.java
@@ -0,0 +1,13 @@
+package com.vinisolon.auditing.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaAuditingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaAuditingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinisolon/auditing/app/SpringBootJpaAuditingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisolon.auditing.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaAuditingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
