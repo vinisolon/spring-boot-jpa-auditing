f176296c9344eb2388565fa6373f22409611b965
Vinicius Solon <vini.97@live.com>
2025-07-04 08:14:10 -0300

    Implements Anime entity

---
+++ src/main/java/com/vinisolon/auditing/app/entities/Anime.java
@@ -0,0 +1,44 @@
+package com.vinisolon.auditing.app.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String title;
+    @Column(nullable = false)
+    private String authorName;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private Integer releaseYear;
+    @Column(nullable = false)
+    private Boolean finished;
+    @Column(nullable = false)
+    private Integer seasons;
+    @CreationTimestamp
+    @Column(columnDefinition = "TIMESTAMP", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    @Column(columnDefinition = "TIMESTAMP", nullable = false)
+    private LocalDateTime updatedAt;
+}
